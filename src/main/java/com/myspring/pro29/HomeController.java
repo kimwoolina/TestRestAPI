5ab11dca40a53ab2b9c87af9d36d7efbd865e739
kimwoolina <rladnfls0500@gmail.com>
2021-07-19 03:11:15 +0900

    게시판 연동

--- src/main/java/com/myspring/pro29/HomeController.java
+++ src/main/java/com/myspring/pro29/HomeController.java
@@ -39,7 +39,7 @@ public class HomeController {
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		return "JSONTest";
+		return "JSONTest2";
 	}
 	
 }
